8a85218acb462617f1273e7680c31fe8b3aee203
irwanesia <irwan.codeir@gmail.com>
2023-12-13 19:42:16 +0700

    add deafault value

--- src/Customer.php
+++ src/Customer.php
@@ -9,7 +9,7 @@ class Customer
 
     }
 
-    public function sayHello(string $name): string
+    public function sayHello(string $name="GUEST"): string
     {
         return "Hello $name, my name is $this->name";
     }
